1091e12f19238ae9da226a77c68f11b4db8921f9
Rahulion06 <96942463+Rahulion06@users.noreply.github.com>
2022-03-18 21:13:13 +0530

    Update __main__.py

--- KazukoBot/__main__.py
+++ KazukoBot/__main__.py
@@ -74,7 +74,7 @@ def get_readable_time(seconds: int) -> str:
 
 
 PM_START_TEXT = """
-Hey there, I'm Kazuko,
+Hey there, I'm Shikimori Micchon,
 
 I am an Anime themed group management bot
 to help maintaining groups with my super advanced features.
@@ -97,7 +97,7 @@ buttons = [
       ],
     [
         InlineKeyboardButton(
-            text="️Add Kazuko to your group", url="t.me/KazukoRobot?startgroup=true",
+            text="️Add Kazuko to your group", url="t.me/ShikimoriMicchonRobot?startgroup=true",
         ),
     ],
     [ 
@@ -107,7 +107,7 @@ buttons = [
 
 
 HELP_STRINGS = """
-Hey there! My name is Kazuko.
+Hey there! My name is Shikimori.
 Have a look at the following for an idea of some of
 the things I can help you with.
 Main commands available:
